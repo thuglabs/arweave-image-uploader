537959ae86c9ec9895c37be403d910dee486ece0
Andy Burkovetsky <jamandyjam@gmail.com>
2021-08-28 18:14:43 +0300

    Add uploader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Arweave from "arweave";
+
+const initOptions = {
+  host: 'arweave.net',// Hostname or IP address for a Arweave host
+  port: 443,          // Port
+  protocol: 'https',  // Network protocol http or https
+  timeout: 20000,     // Network request timeouts in milliseconds
+  logging: false,     // Enable network request logging
+}
+
+const initOptionsLocal = {
+  host: 'localhost',// Hostname or IP address for a Arweave host
+  port: 1984,          // Port
+  protocol: 'http',  // Network protocol http or https
+  timeout: 20000,     // Network request timeouts in milliseconds
+  // logging: false,     // Enable network request logging
+}
+
+window.addEventListener("arweaveWalletLoaded", () => {
+  console.log('arweaveWalletLoaded');
+  /** Handle ArConnect load event **/
+});
 
 function App() {
+  useEffect(() => {
+    const doArweaveStuff = async () => {
+
+      const imageResponse = await fetch('images/0.png');
+      // const image = await imageResponse.json();
+      const blob = await imageResponse.blob();
+      const imageBuffer = blob;
+      const arrayBuffer = await new Response(blob).arrayBuffer();
+      const image = new Uint8Array(imageBuffer);
+      console.log('image', image)
+
+      const arweave = Arweave.init(initOptionsLocal);
+      console.log("arweave", arweave);
+      // arweave.wallets.getBalance('H10jaICIKCRgCer2JIUgMmJFDtjJB49W3kXRH6Adk-I')
+      //   .then((balance) => {
+      //     let winston = balance;
+      //     let ar = arweave.ar.winstonToAr(balance);
+      //     // console.log('hello', winston)
+      //     console.log('winston', winston);
+      //     //125213858712
+
+      //     console.log('ar', ar);
+      //     //0.125213858712
+      //   }
+      // );
+      // let key = await arweave.wallets.generate();
+
+      // Plain text
+      let tx = await arweave.createTransaction(
+        {
+          data: image,
+        }
+      );
+
+      console.log("tx", tx);
+      tx.addTag('Content-Type', 'image/png');
+      // tx.addTag('Content-Type', 'png');
+
+      await arweave.transactions.sign(tx);
+      const result = await arweave.transactions.post(tx);
+      console.log('result', result)
+
+      // console.log(`https://arweave.net/tx/${tx.id}/data`);
+      //www.arweave.net/aQmNwDsDgo7i2LOAdOQjMu9_V-VsN4UcYsFZ-ZWWLqE?ext=png",
+
+      arweave.transactions.getStatus(tx.id).then(res => {
+        console.log('res', res);
+        // {
+        //  status: 200,
+        //  confirmed: {
+        //    block_height: 140151,
+        //    block_indep_hash: 'OR1wue3oBSg3XWvH0GBlauAtAjBICVs2F_8YLYQ3aoAR7q6_3fFeuBOw7d-JTEdR',
+        //    number_of_confirmations: 20
+        //  }
+        //}
+    })
+
+      // const result = await arweave.api.get(tx.id);
+      // const data = await result.json();
+      // console.log("data", data);
+
+      // Buffer
+      // let transactionB = await arweave.createTransaction(
+      //   {
+      //     data: Buffer.from("Some data", "utf8"),
+      //   },
+      //   key
+      // );
+
+      // console.log('transactionB', transactionB);
+    };
+
+    doArweaveStuff();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +116,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <img src="images/0.png" /> */}
       </header>
     </div>
   );
---
+++ uploader.js
@@ -0,0 +1,85 @@
+import fs from "fs";
+import path from "path";
+import Arweave from "arweave";
+
+const initOptions = {
+  host: "arweave.net", // Hostname or IP address for a Arweave host
+  port: 443, // Port
+  protocol: "https", // Network protocol http or https
+  timeout: 20000, // Network request timeouts in milliseconds
+  logging: false, // Enable network request logging
+};
+
+// run
+// npx @textury/arlocal
+const initOptionsLocal = {
+  host: "localhost", // Hostname or IP address for a Arweave host
+  port: 1984, // Port
+  protocol: "http", // Network protocol http or https
+  timeout: 20000, // Network request timeouts in milliseconds
+  // logging: false,     // Enable network request logging
+};
+
+const runUpload = async (fullPath) => {
+  const arweave = Arweave.init(initOptionsLocal);
+
+  const key = await arweave.wallets.generate();
+
+  const data = fs.readFileSync(fullPath);
+
+  const tx = await arweave.createTransaction({ data: data }, key);
+
+  tx.addTag("Content-Type", "image/png");
+
+  await arweave.transactions.sign(tx, key);
+
+  const uploader = await arweave.transactions.getUploader(tx);
+
+  while (!uploader.isComplete) {
+    await uploader.uploadChunk();
+    console.log(
+      `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
+    );
+  }
+
+  await arweave.transactions.post(tx);
+  //   console.log("result", result);
+  console.log("tx", tx);
+  console.log("url", `http://localhost:1984/${tx.id}`);
+  console.log("url", `https://arweave.net/${tx.id}`);
+  // return tx;
+};
+
+const folder = "./public/images";
+const imgWithUrl = {};
+
+const iterateFolderFiles = async () => {
+  try {
+    // Get the files as an array
+    const files = await fs.promises.readdir(folder);
+
+    // Loop them all with the new for...of
+    for (const file of files) {
+      // Get the full paths
+      const fullPath = path.join(folder, file);
+      const fileName = file.replace(".png", "");
+      console.log("file", file);
+      console.log("fileName", fileName);
+      console.log("fullPath", fullPath);
+      //   console.log('data', data);
+      const { id } = runUpload(fullPath);
+      imgWithUrl = {
+        ...imgWithUrl,
+        [fileName]: id,
+      };
+    } // End for...of
+
+    // All images iterated
+    // save imgWithUrl to json
+  } catch (e) {
+    // Catch anything bad that happens
+    console.error("We've thrown! Whoops!", e);
+  }
+};
+
+iterateFolderFiles();
